9912390f2b3b34725f6f864207452b904d659ab9
Jaevent <114322700+Jaevent@users.noreply.github.com>
2022-11-27 13:50:34 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -4,4 +4,17 @@ set -e
 
 rm -rf student-submission
 git clone $1 student-submission
-
+cd student-submission
+if[[-e "ListExamples.java" ]]
+then
+    echo "ListExamples.java found! full credit!"
+else
+    echo "ListExamples.java is missing! no credit."
+fi
+logout
+cp GradeServer.java Server.java TestListExamples.java student-submission
+javac *.java
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+set +e
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples.java
+echo $?
